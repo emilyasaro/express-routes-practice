4c48ecc09f4b3da5400a29306c63b06e3889ccd4
Emily <emilypearlasaro@gmail.com>
2021-02-21 14:53:09 -0500

    server.js not recognizing db, syncAndSeed, User

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_users_db', {logging: false});
+const { db, syncAndSeed, User } = require('./db/index.js');
+console.log('***',User)
+const express = require('express');
 
-const User = db.define('User', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      isEmail: true
-    }
+const app = express();
+
+app.get('/users', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.send(users);
+  }
+  catch (error) {
+    next(error)
   }
 })
-User.beforeSave( user => {
-  if (!user.name) {
-    user.name =  `${user.email}'s name is ${faker.lorem.firstName(3)}`
-  }
-})
-
-const syncAndSeed = async () => {
-  await db.sync({ force: true });
-  await User.create({ name: 'moe', email: 'moe@gmail.com' });
-  await User.create({ name: 'ethel', email: 'ethel@gmail.com' });
-  await User.create({ name: 'lucy', email: 'lucy@gmail.com' });
-}
-
 const init = async () => {
-  await db.authenticate(); // will confirm that we're connected to the db
-  await syncAndSeed();
-  console.log(await User.findAll())
+  try {
+    // await db.authenticate(); // will confirm that we're connected to the db
+    await syncAndSeed();
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`app listening on port ${por}`))}
+  catch(e) {
+    console.log(e)
+  }
 }
 init();
